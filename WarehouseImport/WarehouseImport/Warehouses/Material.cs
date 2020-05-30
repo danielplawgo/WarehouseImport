8f4878418ec0591a0ba0db9fefb3477389372ec5
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 13:19:58 +0200

    add model for warehouse and material.

---
+++ WarehouseImport/WarehouseImport/Warehouses/Material.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Warehouses
+{
+    public class Material
+    {
+        public Material(string name, string id)
+        {
+            Name = name;
+            Id = id;
+            Count = 0;
+        }
+
+        public string Name { get; }
+
+        public string Id { get; }
+
+        public int Count { get; private set; }
+    }
+}
---
+++ WarehouseImport/WarehouseImport/Warehouses/Warehouse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Warehouses
+{
+    public class Warehouse
+    {
+        private readonly List<Material> _materials = new List<Material>();
+
+        public Warehouse(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public IEnumerable<Material> Materials => _materials;
+    }
+}
