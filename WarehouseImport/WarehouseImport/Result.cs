90a59e44cb1230bab615597690974c48b1910aa1
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 08:10:18 +0200

    add class for results

---
+++ WarehouseImport/WarehouseImport/Result.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+
+        public IEnumerable<ErrorMessage> Errors { get; set; }
+
+        public static Result Ok()
+        {
+            return new Result()
+            {
+                Success = true
+            };
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>()
+            {
+                Success = true,
+                Value = value
+            };
+        }
+
+        public static Result<T> Failure<T>(string message)
+        {
+            var result = new Result<T>();
+
+            result.Success = false;
+            result.Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = message
+                }
+            };
+
+            return result;
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; set; }
+    }
+
+    public class ErrorMessage
+    {
+        public string PropertyName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
