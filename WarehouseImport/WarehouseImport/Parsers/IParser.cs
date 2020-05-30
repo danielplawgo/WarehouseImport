eb1469f13034ca36736220afd85379e974770fc1
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 08:13:15 +0200

    Add IParser interface

---
+++ WarehouseImport/WarehouseImport/Parsers/IParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Parsers
+{
+    public interface IParser
+    {
+        Result<ICommand> Parse(string line);
+    }
+}
