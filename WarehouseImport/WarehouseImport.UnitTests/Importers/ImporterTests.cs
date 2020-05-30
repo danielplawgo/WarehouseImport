09e359249bd8b7969fcc8599142202d27273fe92
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 09:24:06 +0200

    add test class for importer

---
+++ WarehouseImport/WarehouseImport.UnitTests/Importers/ImporterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehouseImport.Importers;
+
+namespace WarehouseImport.UnitTests.Importers
+{
+    public class ImporterTests
+    {
+        protected virtual Importer Create()
+        {
+            return new Importer();
+        }
+    }
+}
