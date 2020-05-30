69ae0fcaec2f649738d71846340697274db826c2
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 09:14:56 +0200

    add interface for import data source

---
+++ WarehouseImport/WarehouseImport/Importer/IImportSource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Importer
+{
+    public interface IImportSource
+    {
+        IEnumerable<string> GetLines();
+    }
+}
