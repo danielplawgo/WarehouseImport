2b2354f296ba082529d8835961feff8aeb37cf56
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 08:22:21 +0200

    add unit tests class for default parser

---
+++ WarehouseImport/WarehouseImport.UnitTests/Parsers/DefaultParserTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehouseImport.Parsers;
+
+namespace WarehouseImport.UnitTests.Parsers
+{
+    public class DefaultParserTests
+    {
+        protected virtual DefaultParser Create()
+        {
+            return new DefaultParser();
+        }
+    }
+}
