2c3252cf954f063e5c2db59b65705dfca841ea61
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 08:14:31 +0200

    add default parser

---
+++ WarehouseImport/WarehouseImport/Parsers/DefaultParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Parsers
+{
+    public class DefaultParser : IParser
+    {
+        public Result<ICommand> Parse(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
