4d0fde3405db6ad389e4f56c3533d66b0c5e4133
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 09:18:54 +0200

    add empty importer class

---
+++ WarehouseImport/WarehouseImport/Importer/Importer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Importer
+{
+    public class Importer : IImporter
+    {
+        public Result Import()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
