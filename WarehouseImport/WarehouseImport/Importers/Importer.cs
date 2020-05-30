c4017b8f5b47a79106512a2abfaa23a3503e97d4
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 10:31:31 +0200

    add logic for parsing line

--- WarehouseImport/WarehouseImport/Importers/Importer.cs
+++ WarehouseImport/WarehouseImport/Importers/Importer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using WarehouseImport.Parsers;
 
 namespace WarehouseImport.Importers
 {
@@ -8,14 +10,25 @@ namespace WarehouseImport.Importers
     {
         private readonly IImportSource _importSource;
 
-        public Importer(IImportSource importSource)
+        private readonly IParser[] _parsers;
+
+        public Importer(IImportSource importSource,
+            IParser[] parsers)
         {
             _importSource = importSource;
+            _parsers = parsers;
         }
 
         public Result Import()
         {
             var lines = _importSource.GetLines();
+
+            var parser = _parsers.FirstOrDefault(); //we can add some logic for getting correct parser
+
+            foreach (var line in lines)
+            {
+                var commandResult = parser.Parse(line);
+            }
             
             return Result.Ok();
         }
