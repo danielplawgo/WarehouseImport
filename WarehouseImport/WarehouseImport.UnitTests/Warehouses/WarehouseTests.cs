8a6846e03936ff938a08c45e515dd356b17b207a
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 13:26:55 +0200

    add test for creating empty warehouse

---
+++ WarehouseImport/WarehouseImport.UnitTests/Warehouses/WarehouseTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using WarehouseImport.Warehouses;
+using Xunit;
+
+namespace WarehouseImport.UnitTests.Warehouses
+{
+    public class WarehouseTests
+    {
+        [Fact]
+        public void Create_New_Warehouse()
+        {
+            var warehouse = new Warehouse("warehouse");
+
+            warehouse.Name
+                .Should()
+                .Be("warehouse");
+
+            warehouse.Materials
+                .Should()
+                .BeEmpty();
+        }
+    }
+}
