26b9cd799dfe6235205870350bf5b71803ccc883
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 09:07:44 +0200

    add logic for parsing correct data

--- WarehouseImport/WarehouseImport/Parsers/DefaultParser.cs
+++ WarehouseImport/WarehouseImport/Parsers/DefaultParser.cs
@@ -18,7 +18,34 @@ namespace WarehouseImport.Parsers
                 return Result.Ok<ICommand>(new NullCommand());
             }
 
-            throw new NotImplementedException();
+            var command = new MaterialImportCommand();
+
+            var data = line.Split(";");
+
+            command.Name = data[0];
+            command.Id = data[1];
+
+            var warehouseData = data[2].Split("|");
+            var warehouses = new List<MaterialImportCommand.WarehouseCount>();
+
+            foreach (var item in warehouseData)
+            {
+                var itemData = item.Split(",");
+
+                var warehouseCount = new MaterialImportCommand.WarehouseCount();
+                warehouseCount.Name = itemData[0];
+                
+                if(int.TryParse(itemData[1], out var count))
+                {
+                    warehouseCount.Count = count;
+                }
+                
+                warehouses.Add(warehouseCount);
+            }
+
+            command.Warehouses = warehouses;
+
+            return Result.Ok<ICommand>(command);
         }
     }
 }
