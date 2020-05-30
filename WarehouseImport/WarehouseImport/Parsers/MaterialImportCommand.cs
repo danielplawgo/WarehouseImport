4d988f6bd02a872994589debddf8d72aa3e3b769
Daniel Plawgo <daniel@plawgo.pl>
2020-05-30 08:53:07 +0200

    add command for import warehouse data

---
+++ WarehouseImport/WarehouseImport/Parsers/MaterialImportCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Parsers
+{
+    public class MaterialImportCommand : ICommand
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public IEnumerable<WarehouseCount> Warehouses { get; set; }
+
+        public class WarehouseCount
+        {
+            public string Name { get; set; }
+
+            public int Count { get; set; }
+        }
+    }
+}
