8bc65db06b42e9ac849e0fe9b7ae74971e9efc4b
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 07:13:04 +0200

    add class for testing material import handler

---
+++ WarehouseImport/WarehouseImport.UnitTests/Importers/MaterialImportHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehouseImport.Importers;
+using WarehouseImport.Parsers;
+
+namespace WarehouseImport.UnitTests.Importers
+{
+    public class MaterialImportHandlerTests
+    {
+        protected MaterialImportCommand Command = new MaterialImportCommand()
+        {
+            Id = "id",
+            Name = "name",
+            Warehouses = new List<MaterialImportCommand.WarehouseCount>()
+            {
+                new MaterialImportCommand.WarehouseCount()
+                {
+                    Name = "warehouse",
+                    Count = 10
+                }
+            }
+        };
+
+        protected virtual MaterialImportHandler Create()
+        {
+            return new MaterialImportHandler();
+        }
+    }
+}
