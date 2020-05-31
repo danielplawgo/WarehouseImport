069fb19b0bd64addbfa7df8ad80145a9093e471e
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 09:40:47 +0200

    add empty class for export format tests

---
+++ WarehouseImport/WarehouseImport.UnitTests/Exporters/WarehouseExportFormatterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehouseImport.Exporters;
+
+namespace WarehouseImport.UnitTests.Exporters
+{
+    public class WarehouseExportFormatterTests
+    {
+        protected virtual WarehouseExportFormatter Create()
+        {
+            return new WarehouseExportFormatter();
+        }
+    }
+}
