f43ba3f8310a0f2fffdc3d1aaead3c39ca351b64
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 09:03:26 +0200

    add empty class for export handler tests

---
+++ WarehouseImport/WarehouseImport.UnitTests/Exporters/ExportHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using WarehouseImport.Exporters;
+using WarehouseImport.Warehouses;
+
+namespace WarehouseImport.UnitTests.Exporters
+{
+    public class ExportHandlerTests
+    {
+        protected Mock<IWarehouseRepository> Repository;
+
+        protected List<Warehouse> Warehouses = new List<Warehouse>();
+
+        protected virtual ExportHandler Create()
+        {
+            Repository = new Mock<IWarehouseRepository>();
+            Repository.SetupGet(m => m.Warehouses)
+                .Returns(Warehouses);
+
+            return new ExportHandler(Repository.Object);
+        }
+    }
+}
--- WarehouseImport/WarehouseImport/Exporters/ExportHandler.cs
+++ WarehouseImport/WarehouseImport/Exporters/ExportHandler.cs
@@ -4,11 +4,19 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using WarehouseImport.Warehouses;
 
 namespace WarehouseImport.Exporters
 {
     public class ExportHandler : IRequestHandler<ExportQuery, Result<IEnumerable<ExportQuery.WarehouseDto>>>
     {
+        private readonly IWarehouseRepository _repository;
+
+        public ExportHandler(IWarehouseRepository repository)
+        {
+            _repository = repository;
+        }
+
         public Task<Result<IEnumerable<ExportQuery.WarehouseDto>>> Handle(ExportQuery request, CancellationToken token)
         {
             throw new NotImplementedException();
