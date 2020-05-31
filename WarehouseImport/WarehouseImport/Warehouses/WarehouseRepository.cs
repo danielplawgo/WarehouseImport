e1a1a9cf28eab4fa256ef036229f6ef9a265a38a
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 06:51:01 +0200

    add empty warehouse repository

---
+++ WarehouseImport/WarehouseImport/Warehouses/WarehouseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseImport.Warehouses
+{
+    public class WarehouseRepository : IWarehouseRepository
+    {
+        private readonly List<Warehouse> _warehouses = new List<Warehouse>();
+        public IEnumerable<Warehouse> Warehouses => _warehouses;
+    }
+
+    public interface IWarehouseRepository
+    {
+        IEnumerable<Warehouse> Warehouses { get; }
+    }
+}
