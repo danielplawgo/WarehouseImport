c880a39d4a7f8b2194bfcce4a4f82150c8f81e96
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 07:10:20 +0200

    add empty material import request handler

---
+++ WarehouseImport/WarehouseImport/Importers/MaterialImportHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using WarehouseImport.Parsers;
+
+namespace WarehouseImport.Importers
+{
+    public class MaterialImportHandler : IRequestHandler<MaterialImportCommand, Result>
+    {
+        public Task<Result> Handle(MaterialImportCommand request, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
