0ee463ce7ac298f011202227ee905f953dd92b7c
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 09:21:33 +0200

    add logic for ordering results by warehouse count

--- WarehouseImport/WarehouseImport/Exporters/ExportHandler.cs
+++ WarehouseImport/WarehouseImport/Exporters/ExportHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,7 +20,15 @@ namespace WarehouseImport.Exporters
 
         public Task<Result<IEnumerable<ExportQuery.WarehouseDto>>> Handle(ExportQuery request, CancellationToken token)
         {
-            throw new NotImplementedException();
+            var result = _repository.Warehouses
+                .OrderByDescending(w => w.Count)
+                .Select(w => new ExportQuery.WarehouseDto()
+                {
+                    Name = w.Name,
+                    Count = w.Count
+                });
+
+            return Result.OkAsync(result);
         }
     }
 }
