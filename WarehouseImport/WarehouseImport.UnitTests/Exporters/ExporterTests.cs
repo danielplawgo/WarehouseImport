4bfc28375de010adcb006afe2d0533edfd0c201e
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 10:03:29 +0200

    add empty class for exporter tests

---
+++ WarehouseImport/WarehouseImport.UnitTests/Exporters/ExporterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehouseImport.Exporters;
+
+namespace WarehouseImport.UnitTests.Exporters
+{
+    public class ExporterTests
+    {
+        protected virtual Exporter Create()
+        {
+            return new Exporter();
+        }
+    }
+}
