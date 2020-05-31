bd76897b8d28af45872663d1494d8506646aaffa
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 11:24:45 +0200

    add handler for null command

---
+++ WarehouseImport/WarehouseImport/Commands/NullCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace WarehouseImport.Commands
+{
+    public class NullCommandHandler : IRequestHandler<NullCommand, Result>
+    {
+        public Task<Result> Handle(NullCommand request, CancellationToken cancellationToken)
+        {
+            return Result.OkAsync();
+        }
+    }
+}
