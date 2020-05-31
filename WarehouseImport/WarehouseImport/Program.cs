7ba42cfb9c6ab6a215c254c0a1dbaee1ca0506ee
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 10:50:55 +0200

    add method for creating autofac container

--- WarehouseImport/WarehouseImport/Program.cs
+++ WarehouseImport/WarehouseImport/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Autofac;
 
 namespace WarehouseImport
 {
@@ -7,6 +8,17 @@ namespace WarehouseImport
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(typeof(Program).Assembly)
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            return builder.Build();
         }
     }
 }
