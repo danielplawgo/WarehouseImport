71e4a2950c6ff2f5f731faf04a894991448a45ce
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 10:44:49 +0200

    add test for running import

--- WarehouseImport/WarehouseImport.UnitTests/ApplicationTests.cs
+++ WarehouseImport/WarehouseImport.UnitTests/ApplicationTests.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Moq;
+using WarehouseImport.Importers;
+using Xunit;
 
 namespace WarehouseImport.UnitTests
 {
     public class ApplicationTests
     {
+        protected Mock<IImporter> Importer;
+
         protected virtual Application Create()
         {
-            return new Application();
+            Importer = new Mock<IImporter>();
+            return new Application(Importer.Object);
+        }
+
+        [Fact]
+        public async void Run_Import()
+        {
+            var application = Create();
+
+            await application.RunAsync();
+
+            Importer.Verify(m => m.ImportAsync(), Times.Once);
         }
     }
 }
