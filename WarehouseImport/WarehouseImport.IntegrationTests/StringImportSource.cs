aa5b2e004661b7157f538b0e72e8914790d97bef
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 11:42:39 +0200

    add import source implementation for integration tests

---
+++ WarehouseImport/WarehouseImport.IntegrationTests/StringImportSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseImport.Importers;
+
+namespace WarehouseImport.IntegrationTests
+{
+    public class StringImportSource : IImportSource
+    {
+        private string _data;
+
+        public void Init(string data)
+        {
+            _data = data;
+        }
+
+        public Task<IEnumerable<string>> GetLinesAsync()
+        {
+            return Task.FromResult(_data.Split(Environment.NewLine) as IEnumerable<string>);
+        }
+    }
+}
