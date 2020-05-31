8a71253457d59da58410b1a17e22005e7758fb30
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 08:11:11 +0200

    add export query

---
+++ WarehouseImport/WarehouseImport/Exporters/ExportQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace WarehouseImport.Exporters
+{
+    public class ExportQuery : IQuery, IRequest<Result<IEnumerable<ExportQuery.WarehouseDto>>>
+    {
+        public class WarehouseDto
+        {
+            public string Name { get; set; }
+
+            public int Count { get; set; }
+        }
+
+        public class MaterialDto
+        {
+            public string Id { get; set; }
+
+            public int Count { get; set; }
+        }
+    }
+}
