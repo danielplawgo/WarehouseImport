cd0a79e627893571d3f216a939ca2573df852366
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 12:00:59 +0200

    add export destination to console

---
+++ WarehouseImport/WarehouseImport/Console/ConsoleExportDestination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseImport.Exporters;
+
+namespace WarehouseImport.Console
+{
+    public class ConsoleExportDestination : IExportDestination
+    {
+        public Task WriteAsync(string value)
+        {
+            System.Console.WriteLine(value);
+
+            return Task.CompletedTask;
+        }
+    }
+}
