38ae5d5bebb2eff9c2d35abc4aad14f28a2811f7
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 10:25:33 +0200

    add interfejs for export destination

---
+++ WarehouseImport/WarehouseImport/Exporters/IExportDestination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseImport.Exporters
+{
+    public interface IExportDestination
+    {
+        Task WriteAsync(string value);
+    }
+}
