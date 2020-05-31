9ba72847087a133a9588171a520eb11e808ad08f
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 12:00:36 +0200

    add import source from console

---
+++ WarehouseImport/WarehouseImport/Console/ConsoleImportSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseImport.Importers;
+
+namespace WarehouseImport.Console
+{
+    public class ConsoleImportSource : IImportSource
+    {
+        public Task<IEnumerable<string>> GetLinesAsync()
+        {
+            var result = new List<string>();
+
+            while (true)
+            {
+                var line = System.Console.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                result.Add(line);
+            }
+
+            return Task.FromResult(result as IEnumerable<string>);
+        }
+    }
+}
