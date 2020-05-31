1ca54abbdde4191d1227f230c082669acf94644d
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 11:43:15 +0200

    add export destination implementation for integration tests

---
+++ WarehouseImport/WarehouseImport.IntegrationTests/StringExportDestination.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseImport.Exporters;
+
+namespace WarehouseImport.IntegrationTests
+{
+    public class StringExportDestination : IExportDestination
+    {
+        public string Data { get; private set; }
+        public Task WriteAsync(string value)
+        {
+            Data += value;
+
+            return Task.CompletedTask;
+        }
+    }
+}
