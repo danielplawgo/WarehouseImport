02c2025c03ab8e3153378d6b6a7127d0e9f8463e
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 09:39:11 +0200

    add export formatter for warehouse data

---
+++ WarehouseImport/WarehouseImport/Exporters/IFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseImport.Exporters
+{
+    public interface IFormatter<T>
+    {
+        Task<string> FormatAsync(T value);
+    }
+}
---
+++ WarehouseImport/WarehouseImport/Exporters/WarehouseExportFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseImport.Exporters
+{
+    public class WarehouseExportFormatter : IFormatter<ExportQuery.WarehouseDto>
+    {
+        public Task<string> FormatAsync(ExportQuery.WarehouseDto value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
