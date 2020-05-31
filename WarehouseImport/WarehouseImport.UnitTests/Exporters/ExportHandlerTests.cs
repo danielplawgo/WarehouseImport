08594031d82e170efc854fc0777c813890e6bf8e
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 09:26:50 +0200

    add test for ordering warehouse by name when count is the same.

--- WarehouseImport/WarehouseImport.UnitTests/Exporters/ExportHandlerTests.cs
+++ WarehouseImport/WarehouseImport.UnitTests/Exporters/ExportHandlerTests.cs
@@ -43,6 +43,25 @@ namespace WarehouseImport.UnitTests.Exporters
             return result;
         }
 
+        protected List<Warehouse> CreateWarehousesWithTheSameCounts()
+        {
+            var result = new List<Warehouse>();
+
+            var warehouse = new Warehouse("warehouse1");
+            warehouse.AddMaterial("material1.1", "id1.1", 10);
+            warehouse.AddMaterial("material1.2", "aid1.2", 30);
+
+            result.Add(warehouse);
+
+            warehouse = new Warehouse("zarehouse2");
+            warehouse.AddMaterial("material2.1", "id2.1", 10);
+            warehouse.AddMaterial("material2.2", "aid2.2", 30);
+
+            result.Add(warehouse);
+
+            return result;
+        }
+
         [Fact]
         public async void Order_By_Warehouse_Count_Descending()
         {
@@ -69,6 +88,26 @@ namespace WarehouseImport.UnitTests.Exporters
             secondItem.Count
                 .Should()
                 .Be(40);
+        }
+
+        [Fact]
+        public async void Order_By_Warehouse_Name_When_Count_Is_The_Same()
+        {
+            var hander = Create(CreateWarehousesWithTheSameCounts());
+
+            var result = await hander.Handle(new ExportQuery(), CancellationToken.None);
+
+            var firstItem = result.Value.ElementAt(0);
+
+            firstItem.Name
+                .Should()
+                .Be("zarehouse2");
+
+            var secondItem = result.Value.ElementAt(1);
+
+            secondItem.Name
+                .Should()
+                .Be("warehouse1");
         }
     }
 }
