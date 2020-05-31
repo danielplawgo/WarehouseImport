208d59dbb525776eba306b4c8e2660f186bbc32f
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 10:40:52 +0200

    add empty application class.

---
+++ WarehouseImport/WarehouseImport/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseImport
+{
+    public class Application : IApplication
+    {
+        public Task RunAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IApplication
+    {
+        Task RunAsync();
+    }
+}
