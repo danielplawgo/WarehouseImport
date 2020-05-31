2dc3b68b06a60dbb92381761e1909c04790094cb
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 06:54:02 +0200

    add test for scenario when repository is empty after creation

---
+++ WarehouseImport/WarehouseImport.UnitTests/Warehouses/WarehouseRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using WarehouseImport.Warehouses;
+using Xunit;
+
+namespace WarehouseImport.UnitTests.Warehouses
+{
+    public class WarehouseRepositoryTests
+    {
+        protected virtual WarehouseRepository Create()
+        {
+            return new WarehouseRepository();
+        }
+
+        [Fact]
+        public void Repository_Is_Empty_After_Creation()
+        {
+            var repository = Create();
+
+            repository.Warehouses
+                .Should()
+                .BeEmpty();
+        }
+    }
+}
