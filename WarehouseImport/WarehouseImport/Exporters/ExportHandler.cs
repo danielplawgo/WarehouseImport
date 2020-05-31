3d264ee40706b71d3c22a1b70a3b8804f4de5197
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 08:24:37 +0200

    add empty handler for export query

---
+++ WarehouseImport/WarehouseImport/Exporters/ExportHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace WarehouseImport.Exporters
+{
+    public class ExportHandler : IRequestHandler<ExportQuery, Result<IEnumerable<ExportQuery.WarehouseDto>>>
+    {
+        public Task<Result<IEnumerable<ExportQuery.WarehouseDto>>> Handle(ExportQuery request, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
