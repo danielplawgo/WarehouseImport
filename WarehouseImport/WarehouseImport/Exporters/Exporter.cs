9903609f9775f114662a97cee1097bb6673f0a6b
Daniel Plawgo <daniel@plawgo.pl>
2020-05-31 09:56:36 +0200

    add empty exporter

---
+++ WarehouseImport/WarehouseImport/Exporters/Exporter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseImport.Exporters
+{
+    public class Exporter : IExporter
+    {
+        public Task<Result> ExportAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WarehouseImport/WarehouseImport/Exporters/IExporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseImport.Exporters
+{
+    public interface IExporter
+    {
+        Task<Result> ExportAsync();
+    }
+}
